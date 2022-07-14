2e310d5afcdf30ec255ea70583ef8b2c09b5727b
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-07-14 13:28:36 -0300

    add: icons

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                        source={{uri: 'https://avatars.githubusercontent.com/u/65553463?v=4'}}
+                        
+                        />
+
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Juathan</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name='power' />
+                </UserWrapper>
+            </Header>
         </Container>
     );
-}
+}   
