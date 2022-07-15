90c111448a4342ac723fc56bdc4fe3f673970e09
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-07-14 21:04:27 -0300

    fix: highlight cards

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -7,20 +7,48 @@ import {
     Icon,
     Footer,
     Amount,
-    LastTransaction } from './styles';
+    LastTransaction 
+} from './styles';
 
-export function HighlightCard() {
+interface Props {
+    title: string;
+    amount: string;
+    lastTransaction: string;
+    type: 'up' | 'down' | 'total';
+}
+
+const icon = {
+    up: 'arrow-up-circle',
+    down: 'arrow-down-circle',
+    total: 'dollar-sign'
+}
+
+export function HighlightCard({ 
+  type,
+  title, 
+  amount, 
+  lastTransaction 
+}: Props) {
   return (
-    <Container>
-        <Header>
-            <Title>Entrada</Title>
-            <Icon name='arrow-up-circle' />
-        </Header>
+    <Container type={type}>
+      <Header>
+        <Title type={type}>
+          {title}
+        </Title>
+        <Icon 
+          name={icon[type]} 
+          type={type}
+        />
+      </Header>
 
-        <Footer>
-            <Amount>R$ 17.400,00</Amount>
-            <LastTransaction>Última entrada dia 13 de abril.</LastTransaction>
-        </Footer>
+      <Footer>
+        <Amount type={type}>
+          {amount}
+        </Amount>
+        <LastTransaction type={type}>
+          {lastTransaction}
+        </LastTransaction>
+      </Footer>
     </Container>
   );
 }
